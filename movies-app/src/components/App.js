bc13070a1afda140e5c9192f829e92d18704b289
zanetagorska <zaneta.gorska1@gmail.com>
2018-12-20 17:30:38 +0100

    Create separate Login view

--- movies-app/src/components/App.js
+++ movies-app/src/components/App.js
@@ -1,52 +1,10 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { login, resetLogin } from '../redux/auth/reducer';
+import React, { Component } from 'react';
+import Login from '../views/login/Login';
 
 class App extends Component {
-	state = {
-		login: '',
-		password: ''
-	};
-
-	handleSubmit = (e) => {
-		e.preventDefault();
-		this.props.login(this.state);
-	};
-
-	handleChange = (field) => {
-		if (this.props.errorMessage) {
-			this.props.resetLogin();
-		}
-
-		this.setState(field);
-	};
-
 	render() {
-		return (
-			<Fragment>
-				<form onSubmit={this.handleSubmit}>
-					<input
-						type="email"
-						value={this.state.login}
-						placeholder="login"
-						onChange={(e) => this.handleChange({ login: e.target.value })}
-					/>
-					<input
-						type="password"
-						value={this.state.password}
-						placeholder="password"
-						onChange={(e) => this.handleChange({ password: e.target.value })}
-					/>
-					<button type="submit">Zaloguj</button>
-				</form>
-				{this.props.errorMessage}
-			</Fragment>
-		);
+		return <Login />;
 	}
 }
 
-const mapStateToProps = (state) => ({
-	errorMessage: state.auth.errorMessage
-});
-
-export default connect(mapStateToProps, { login, resetLogin })(App);
+export default App;
---
+++ movies-app/src/views/login/Login.js
@@ -0,0 +1,58 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { login, resetLogin } from '../../redux/auth/reducer';
+import PropTypes from 'prop-types';
+
+class Login extends Component {
+	state = {
+		login: '',
+		password: ''
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.login(this.state);
+	};
+
+	handleChange = (field) => {
+		if (this.props.errorMessage) {
+			this.props.resetLogin();
+		}
+		this.setState(field);
+	};
+
+	render() {
+		return (
+			<Fragment>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="email"
+						value={this.state.login}
+						placeholder="login"
+						onChange={(e) => this.handleChange({ login: e.target.value })}
+					/>
+					<input
+						type="password"
+						value={this.state.password}
+						placeholder="password"
+						onChange={(e) => this.handleChange({ password: e.target.value })}
+					/>
+					<button type="submit">Zaloguj</button>
+				</form>
+				{this.props.errorMessage}
+			</Fragment>
+		);
+	}
+}
+
+Login.propTypes = {
+	errorMessage: PropTypes.string,
+	login: PropTypes.func,
+	resetLogin: PropTypes.func
+};
+
+const mapStateToProps = (state) => ({
+	errorMessage: state.auth.errorMessage
+});
+
+export default connect(mapStateToProps, { login, resetLogin })(Login);
